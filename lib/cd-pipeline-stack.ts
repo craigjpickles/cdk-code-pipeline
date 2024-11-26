614d8600469e2a6fcf57edb727574f2548a22cc1
craigjpickles <craig.pickles@purple.telstra.com>
2024-11-26 12:57:31 +0800

    CD

--- lib/cd-pipeline-stack.ts
+++ lib/cd-pipeline-stack.ts
@@ -1,47 +1,28 @@
 import * as cdk from 'aws-cdk-lib/core'
 import * as codebuild from 'aws-cdk-lib/aws-codebuild'
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline'
-import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions'
+import {
+  CodeBuildAction,
+  CodeStarConnectionsSourceAction,
+  ManualApprovalAction,
+} from 'aws-cdk-lib/aws-codepipeline-actions'
 import * as iam from 'aws-cdk-lib/aws-iam'
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as kms from 'aws-cdk-lib/aws-kms'
 import * as ssm from 'aws-cdk-lib/aws-ssm' // Add this line
 import { Construct } from 'constructs'
 import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild'
+import { Environment } from 'aws-cdk-lib/aws-appconfig'
 
 export class CDPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-
-    const stackName = new cdk.CfnParameter(this, 'StackName', {
-      type: 'String',
-      description: 'lowercase name of stack',
-    })
-
-    const repositoryName = new cdk.CfnParameter(this, 'RepositoryName', {
-      type: 'String',
-      description: 'Name of the Project',
-    })
-
-    const repositoryOwner = new cdk.CfnParameter(this, 'RepositoryOwner', {
-      type: 'String',
-      description: 'Repository Owner',
-    })
 
     const branchName = new cdk.CfnParameter(this, 'BranchName', {
       type: 'String',
       description: 'Name of the branch',
       default: 'main',
     })
-
-    const codeStarConnectionId = new cdk.CfnParameter(
-      this,
-      'CodeStarConnectionId',
-      {
-        type: 'String',
-        description: 'The codestar connection ID',
-      },
-    )
 
     const branchTemplateName = new cdk.CfnParameter(
       this,
@@ -50,6 +31,15 @@ export class CDPipelineStack extends cdk.Stack {
         type: 'String',
         description: 'Name of the CF template',
         default: 'template.yaml',
+      },
+    )
+
+    const codeStarConnectionId = new cdk.CfnParameter(
+      this,
+      'CodeStarConnectionId',
+      {
+        type: 'String',
+        description: 'The codestar connection ID',
       },
     )
 
@@ -69,28 +59,45 @@ export class CDPipelineStack extends cdk.Stack {
       },
     )
 
+    const productionAccount = new cdk.CfnParameter(this, 'ProductionAccount', {
+      type: 'Number',
+      description: 'AWS AccountNumber for Production',
+    })
+
+    const repositoryName = new cdk.CfnParameter(this, 'RepositoryName', {
+      type: 'String',
+      description: 'Name of the Project',
+    })
+
+    const repositoryOwner = new cdk.CfnParameter(this, 'RepositoryOwner', {
+      type: 'String',
+      description: 'Repository Owner',
+    })
+
+    const stackName = new cdk.CfnParameter(this, 'StackName', {
+      type: 'String',
+      description: 'lowercase name of stack',
+    })
+
     const artifactBucketName = ssm.StringParameter.valueForStringParameter(
       this,
       '/CodePipelineCDK/CommonResources/S3Bucket',
     )
+
     const encryptionKeyArn = ssm.StringParameter.valueForStringParameter(
       this,
       '/CodePipelineCDK/CommonResources/CMKARN',
     )
+
     const pipelineRoleArn = ssm.StringParameter.valueForStringParameter(
       this,
       '/CodePipelineCDK/CommonResources/PipelineRoleARN',
     )
+
     const buildRoleArn = ssm.StringParameter.valueForStringParameter(
       this,
       '/CodePipelineCDK/CommonResources/BuildProjectRoleARN',
     )
-
-    const crossAccountDeployerRoleName =
-      ssm.StringParameter.valueForStringParameter(
-        this,
-        '/CodePipelineCDK/CommonResources/CrossAccountDeployerRoleName',
-      )
 
     const artifactBucket = s3.Bucket.fromBucketName(
       this,
@@ -109,6 +116,18 @@ export class CDPipelineStack extends cdk.Stack {
       'PipelineRole',
       pipelineRoleArn,
     )
+
+    const crossAccountDeployerRoleName =
+      ssm.StringParameter.valueForStringParameter(
+        this,
+        '/CodePipelineCDK/CommonResources/CrossAccountDeployerRoleName',
+      )
+
+    const crossAccountPipelineRoleName =
+      ssm.StringParameter.valueForStringParameter(
+        this,
+        '/CodePipelineCDK/CommonResources/CrossAccountPipelineRoleName',
+      )
 
     const buildRole = iam.Role.fromRoleArn(this, 'BuildRole', buildRoleArn)
 
@@ -132,6 +151,153 @@ export class CDPipelineStack extends cdk.Stack {
       },
       role: buildRole,
       queuedTimeout: cdk.Duration.minutes(10),
+    })
+
+    // deploy project
+    const deployBuildProject = new codebuild.PipelineProject(
+      this,
+      'DeployBuildProject',
+      {
+        description: `${stackName.valueAsString} - Deploy Build`,
+        encryptionKey: encryptionKey,
+        environment: {
+          computeType: codebuild.ComputeType.SMALL,
+          buildImage: LinuxBuildImage.AMAZON_LINUX_2_5,
+        },
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: '0.2',
+          phases: {
+            build: {
+              commands: `
+            RESPONSE=$(aws sts assume-role --role-arn arn:aws:iam::$AccountNumber:role/$RoleArn --role-session-name $(date "+%Y%m%d_%H%M%S"))
+            export AWS_ACCESS_KEY_ID=$(echo $RESPONSE | jq -r '.Credentials.AccessKeyId')
+            export AWS_SECRET_ACCESS_KEY=$(echo $RESPONSE | jq -r '.Credentials.SecretAccessKey')
+            export AWS_SESSION_TOKEN=$(echo $RESPONSE | jq -r '.Credentials.SessionToken')
+            
+            STACK_NAME="$StackName-$Environment"
+            BRANCH_NAME=$BranchName
+            echo "The branch name is: $BRANCH_NAME"
+
+            SANITISED_BRANCH=$(echo $BRANCH_NAME | sed 's/[\/\\]/-/g')
+            echo "The sanitised branch name is: $SANITISED_BRANCH"
+            MODIFIED_STACK_NAME=$STACK_NAME-$SANITISED_BRANCH
+            echo "Deploying template TemplateName.yaml as stack '$MODIFIED_STACK_NAME'"
+            aws cloudformation deploy --template-file packaged-template.yaml --parameter-overrides file://packaged-template-dev-params.json --stack-name $MODIFIED_STACK_NAME --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+        `,
+            },
+          },
+          artifacts: {
+            files: ['**/*'],
+            'discard-paths': 'yes',
+          },
+        }),
+        role: buildRole,
+        queuedTimeout: cdk.Duration.minutes(10),
+      },
+    )
+
+    const sourceAction = new CodeStarConnectionsSourceAction({
+      actionName: 'App',
+      owner: repositoryOwner.valueAsString,
+      repo: repositoryName.valueAsString,
+      branch: branchName.valueAsString,
+      connectionArn: `arn:aws:codestar-connections:ap-southeast-2:${cdk.Aws.ACCOUNT_ID}:connection/${codeStarConnectionId.valueAsString}`,
+      output: new codepipeline.Artifact('SCCheckoutArtifact'),
+      role: buildRole,
+      runOrder: 1,
+      variablesNamespace: 'SourceVariables',
+      triggerOnPush: true,
+    })
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'CodePipelineCDKDemo-CD-Pipeline',
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+      role: pipelineRole,
+      artifactBucket: artifactBucket,
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+
+        {
+          stageName: 'Build',
+          actions: [
+            new CodeBuildAction({
+              actionName: 'BuildAndPackage',
+              project: buildProject,
+              input: new codepipeline.Artifact('SCCheckoutArtifact'),
+              outputs: [new codepipeline.Artifact('BuildOutput')],
+              role: pipelineRole,
+              runOrder: 1,
+            }),
+          ],
+        },
+      ],
+    })
+
+    // Dev Stage
+    pipeline.addStage({
+      stageName: 'DeployDev',
+      actions: [
+        new CodeBuildAction({
+          actionName: 'Deploy',
+          project: deployBuildProject,
+          input: new codepipeline.Artifact('BuildOutput'),
+          outputs: [new codepipeline.Artifact('DeployDevOutput')],
+          role: pipelineRole,
+          runOrder: 1,
+          environmentVariables: {
+            BranchName: {
+              value: '#{SourceVariables.BranchName}',
+            },
+            AccountNumber: { value: nonProductionAccount.valueAsString },
+            RoleArn: { value: crossAccountDeployerRoleName },
+            StackName: { value: stackName.valueAsString },
+            Environment: { value: 'dev' },
+          },
+        }),
+      ],
+    })
+
+    // ApproveDeployProd Stage
+    const approveDeployProdStage = pipeline.addStage({
+      stageName: 'ApproveDeployProd',
+      actions: [],
+    })
+
+    // Add ManualApprovalAction only if SkipApproval is false
+
+    approveDeployProdStage.addAction(
+      new ManualApprovalAction({
+        actionName: 'ApproveDeployProd',
+        additionalInformation:
+          'Ensure that the associated Change Request for a deployment to Production is approved.',
+      }),
+    )
+
+    // Prod Stage
+    pipeline.addStage({
+      stageName: 'DeployProd',
+      actions: [
+        new CodeBuildAction({
+          actionName: 'Deploy',
+          project: deployBuildProject,
+          input: new codepipeline.Artifact('BuildOutput'),
+          outputs: [new codepipeline.Artifact('DeployProdOutput')],
+          role: pipelineRole,
+          runOrder: 1,
+          environmentVariables: {
+            BranchName: {
+              value: '#{SourceVariables.BranchName}',
+            },
+            AccountNumber: { value: productionAccount.valueAsString },
+            RoleArn: { value: crossAccountDeployerRoleName },
+            StackName: { value: stackName.valueAsString },
+            Environment: { value: 'prod' },
+          },
+        }),
+      ],
     })
   }
 }
